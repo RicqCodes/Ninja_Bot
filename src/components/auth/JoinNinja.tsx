a8fd41d6fd94a87e3ee585f325aa12f938f38173
ricqcodes <princenwakanma1996@gmail.com>
2024-01-26 19:49:24 +0100

    fix rerouting wasn't redirecting after successful authentication use a useEffect hook to monitor when we have a session and then route to protected route

--- src/components/auth/JoinNinja.tsx
+++ src/components/auth/JoinNinja.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 import { Database } from "@/types/supabase";
 import { FcGoogle } from "react-icons/fc";
 import { BsGithub } from "react-icons/bs";
@@ -11,29 +12,38 @@ import {
 } from "@/lib/config";
 import Button from "../ui/Button";
 import { Provider } from "@supabase/supabase-js";
-import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 export default function AuthForm() {
+  const router = useRouter();
+
   const supabase = createClientComponentClient<Database>({
     supabaseUrl: NEXT_PUBLIC_SUPABASE_URL,
     supabaseKey: NEXT_PUBLIC_SUPABASE_ANON_KEY,
   });
 
+  useEffect(() => {
+    const getSession = async () => {
+      const session = await supabase.auth.getSession();
+
+      if (session.data.session) {
+        toast.success("Successfully signed in");
+        router.push("/dashboard");
+      }
+    };
+
+    getSession();
+  }, [router, supabase.auth]);
+
   // Sign in with a third-party provider
   const logIn = async (provider: Provider) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider,
-      options: {
-        queryParams: {
-          access_type: "offline",
-          prompt: "consent",
-        },
-        // redirectTo: redirectUrl,
-      },
     });
 
     if (error) {
-      console.log(error);
+      toast.error("Error during authentication, try again!");
     }
   };
 
